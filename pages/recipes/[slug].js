27f757a26ff1db2ff22f9864827401be1fa3cef2
Alonso Moreno <alonso.moreno@gdpteam.com>
2022-01-11 14:13:41 -0500

    feat: fallback & redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENFUL_SPACE_ID,
@@ -16,7 +17,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -25,6 +26,16 @@ export async function getStaticProps({ params }) {
     content_type: 'recipe',
     'fields.slug': params.slug
   })
+
+  if (!items.length) {
+    return { 
+      redirect: { 
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: { recipe: items[0] },
     revalidate: 1,
@@ -32,6 +43,7 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({recipe}) {
+  if (!recipe) return <Skeleton />
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
   return (
     <div>
