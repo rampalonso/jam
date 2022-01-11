b4c23f82fbc6bbf3018b4c3ee53485438384a497
Alonso Moreno <alonso.moreno@gdpteam.com>
2022-01-11 12:23:15 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function Recipes() {
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENFUL_SPACE_ID,
+    accessToken: process.env.CONTENFUL_ACCESS_KEY,
+  })
+  const response = await client.getEntries({ content_type: 'recipe' })
+  return {
+    props: {
+      recipes: response.items
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}
+      </style>
     </div>
   )
 }
+
